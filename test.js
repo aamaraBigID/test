d2247f319c7676f7cc2ca5fd78ff770002c716d2
aamaraBigID <125814892+aamaraBigID@users.noreply.github.com>
2023-02-22 16:23:45 +0200

    Add files via upload

---
+++ test.js
@@ -0,0 +1,37 @@
+const data = {
+  "data": [
+    {
+      "id": 1,
+      "email": "john.doe@example.com",
+      "ipv6": "2001:0db8:85a3:0000:0000:8a2e:0370:7334",
+      "visa": "4532112345678901"
+    },
+    {
+      "id": 2,
+      "email": "jane.smith@example.com",
+      "ipv6": "2001:0db8:85a3:0000:0000:8a2e:0370:1234",
+      "visa": "4123456789012345"
+    },
+    {
+      "id": 3,
+      "email": "bob.johnson@example.com",
+      "ipv6": "2001:0db8:85a3:0000:0000:8a2e:0370:5678",
+      "visa": "4916123456789012"
+    },
+    {
+      "id": 4,
+      "email": "sara.jones@example.com",
+      "ipv6": "2001:0db8:85a3:0000:0000:8a2e:0370:abcd",
+      "visa": "4556123456789012"
+    },
+    {
+      "id": 5,
+      "email": "samuel.johnson@example.com",
+      "ipv6": "2001:0db8:85a3:0000:0000:8a2e:0370:ef12",
+      "visa": "4488123456789012"
+    }
+  ]
+};
+
+console.log(data);
+
